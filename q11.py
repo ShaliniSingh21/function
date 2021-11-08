661f74878381f1f58e71207fb6a794024e397c67
ShaliniSingh21 <91829539+ShaliniSingh21@users.noreply.github.com>
2021-11-08 16:09:17 +0530

    q11.py third max
    q11.py third max

---
+++ q11.py
@@ -0,0 +1,16 @@
+my_dict = {'a':50, 'b':58, 'c':56,'d':40,'e':100,'f':20}
+max1=0
+max2=0
+max3=0
+l=[]
+for i in my_dict:
+    if my_dict[i]>max1:
+        max1=my_dict[i]
+        print(max1)
+for i in range:
+    if my_dict[i]>max2:
+        max2=my_dict[i]
+    elif my_dict[i]>max3:
+        max3=my_dict[i]  
+        i=i+1
+print(max3)         
